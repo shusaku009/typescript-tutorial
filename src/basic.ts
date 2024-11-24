d78f4895fbac9c97a8c59cc707d08bac73b4c897
shusaku009 <shusaku009@gmail.com>
2024-11-24 11:39:36 +0900

    add: 型定義

---
+++ src/basic.ts
@@ -0,0 +1,14 @@
+const message: string = "Hello World!";
+message.toLowerCase();
+
+const user = {
+  name: "Daniel",
+  age: 26,
+  location: "us",
+};
+
+user.location;
+
+const getFavoriteNumber = (): number => {
+  return 26;
+};
