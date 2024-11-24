186b16a61799f06a3757c3dd6debd327f4a95fd5
shusaku009 <shusaku009@gmail.com>
2024-11-24 14:16:13 +0900

    add: リテラル型

--- src/basic.ts
+++ src/basic.ts
@@ -46,3 +46,25 @@ printCoord({ x: 100, y: 500 });
 // const myCanvas = document.getElementById("main_canvas") as HTMLCanvasElement;
 //
 // const a = expr as any as T;
+//
+let changingString = "Hello World";
+changingString = "Ola Mundo";
+
+changingString;
+
+const constantString = "Hello World";
+constantString;
+
+let x: "hello" = "hello";
+x = "hello";
+
+const printText = (s: string, alignment: "left" | "right" | "center") => {
+  console.log(s, alignment);
+};
+
+printText("Hello, world", "left");
+printText("G'day, mate", "center");
+
+const compare = (a: string, b: string): -1 | 0 | 1 => {
+  return a === b ? 0 : a < b ? -1 : 1;
+};
